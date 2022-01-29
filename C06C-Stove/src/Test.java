08bfcb1da7c0ea8055c392216122a83125f39d9f
natha <natha@GamingMonster>
2022-01-29 11:08:10 -0700

    Adding .gitignore

--- C06C-Stove/src/Test.java
+++ C06C-Stove/src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hey");
 		System.out.println("Java is great");
+		System.out.println("Adding .gitignore");
 	}
 }
